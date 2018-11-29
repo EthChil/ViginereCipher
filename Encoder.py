3f1074f1800ae14ae42048ffc8a5a1882a54d829
EthChil <ethanchilderhose@gmail.com>
2018-11-29 17:55:53 -0500

    Reorded encryption code

--- Encoder.py
+++ Encoder.py
@@ -1,16 +1,17 @@
 alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
-text = raw_input("enter text to be encrypted (no spaces all cap)")
-key = raw_input("enter key to encrypt against (no spaces all cap)")
-cipher = []
-
-keyPointer = 0
-
 def handleRollover(num):
     while(num >= len(alphabet)-1):
         num -= len(alphabet)
 
     return num
+
+
+text = raw_input("enter text to be encrypted (no spaces all cap)")
+key = raw_input("enter key to encrypt against (no spaces all cap)")
+cipher = []
+
+keyPointer = 0
 
 for i in range(len(text)):
    cipher.append(alphabet[handleRollover(alphabet.index(key[keyPointer]) + alphabet.index(text[i]))])
